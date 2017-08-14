a6d5d87fd22b27cccfa7ae5198dd8406e0e02e52
Sumit Karn <sumitkarn12@gmail.com>
2017-08-15 00:03:21 +0530

    Push Notification support added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@
 const Collection = Backbone.Collection.extend();
 let _igcard = null;
 
+toastr.options = {
+	timeOut: 0,
+	extendedTimeOut: 0,
+	closeButton: true,
+	positionClass: "toast-top-full-width"
+}
+
 const IGCard = Backbone.View.extend({
 	el: "#ig-card-modal",
 	template: `<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:38.75502008032129% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/<%= code %>/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank"></a></p> <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A Post by @sumitkarn12</p></div></blockquote>`,
@@ -63,3 +70,48 @@ const Contents = Backbone.View.extend({
 
 let _contents = new Contents();
 _contents.render();
+
+var OneSignal = window.OneSignal || [];
+OneSignal.push(["init", {
+	appId: "4614c7c1-18a2-4cdd-9d3b-f54bdd97e5b7",
+	autoRegister: false,
+	notifyButton: {
+		enable: true,
+		size: "small",
+		theme: "inverse",
+		position: "bottom-left"
+	},
+	allowLocalhostAsSecureOrigin: true
+}]);
+
+OneSignal.push(function() {
+	if ( OneSignal.isPushNotificationsSupported() ) {
+		OneSignal.getNotificationPermission().then(permissionStatus=>{
+			if( permissionStatus =="granted" ) {
+				OneSignal.isPushNotificationsEnabled().then(isEnabled=>{
+					if( !isEnabled ) {
+						OneSignal.registerForPushNotifications();
+						console.error( "Some problem occured in push service." );
+					}
+				})
+			}
+		});
+	} else {
+		toastr.warning( "Browser doesn't support Push Notifications" );
+	}
+	OneSignal.on( "subscriptionChange", ( status )=>{
+		if( status ) {
+			toastr.options.timeOut = 5000;
+			toastr.options.extendedTimeOut = 5000;
+			toastr.success( "Thanks a lot" );
+		} else {
+			toastr.options.timeOut = 0;
+			toastr.options.extendedTimeOut = 0;
+			toastr.warning( "You left PUSH Notification service" );
+		}
+	});
+	OneSignal.setDefaultNotificationUrl("https://www.bhasuri.xyz");
+	OneSignal.setDefaultTitle("Logos designed by @sumitkarn12")
+});
+
+
