5e96b2eefe9aa1e82440ffd3a539c73157353957
Sumit Karn <sumitkarn12@gmail.com>
2017-09-02 13:12:29 +0530

    Cover image changed and new content added

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,39 +45,25 @@ toastr.options = {
 	positionClass: "toast-bottom-full-width"
 }
 
-const IGCard = Backbone.View.extend({
-	el: "#ig-card-modal",
-	template: `<blockquote class="instagram-media" data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:38.75502008032129% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/<%= code %>/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank"></a></p> <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A Post by @bymitthu</p></div></blockquote>`,
-	// template: `<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:38.75502008032129% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div> <p style=" margin:8px 0 0 0; padding:0 4px;"> <a href="https://www.instagram.com/p/<%= code %>/" style=" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;" target="_blank"></a></p> <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">A Post by @bymitthu</p></div></blockquote>`,
-	initialize: function() {
-		this.template = _.template( this.template );
-		this.$el.click(ev=> this.close(ev));
-		return this;
-	},
-	render: function( code ) {
-		this.$el.show();
-		this.$el.find(".ig-card-modal-content").html( this.template({ code: code }) );
-		try {instgrm.Embeds.process(); } catch(e){}
-		return this;
-	},
-	events: {
-		"click .ig-card-modal-close": "close"
-	},
-	close: function( ev ) {
-		history.replaceState( null, null, "/" );
-		this.$el.hide();
-	}
-});
-
 const Contents = Backbone.View.extend({
 	el: ".posts #contents",
-	events: {
-		"click a": "show"
+	initialize: function() {
+		this.lazy = new IntersectionObserver( 	entries => this.lazyLoad( entries ), {threshold: 0.1});
+		this.$el.find(".ig-card").each(( i,v )=>this.lazy.observe( v ));
+		this.$el.find(".ig-card").each(( i,v )=>{
+			let t = $( v );
+			t.find("img").attr( "src", t.find("img").data("src") );
+			this.lazy.unobserve( v );
+			if( i > 5 ) return false;
+		});
 	},
-	show: function( ev ) {
-		ev.preventDefault();
-		app.navigate( $(ev.currentTarget).attr("href"), {trigger: true});
-		return;
+	lazyLoad: function( entries ) {
+		if( entries.length > 4 ) return;
+		entries.forEach(v=>{
+			let t = $( v.target );
+			t.find("img").attr( "src", t.find("img").data("src") );
+			this.lazy.unobserve( v.target );
+		});
 	},
 	render: function() {
 		this.$el.show();
@@ -127,8 +113,7 @@ const App = Backbone.Router.extend({
 		"" : 						"posts",
 		"about" : 					"about",
 		"policy" : 					"policy",
-		"contact" : 					"contact",
-		"p(/:id)" : 					"posts"
+		"contact" : 				"contact"
 	},
 	about: function() {},
 	policy: function() {},
@@ -138,10 +123,6 @@ const App = Backbone.Router.extend({
 	posts: function( id ) {
 		if( !_contents ) _contents = new Contents();
 		_contents.render();
-		if( id ) {
-			if( !_igcard ) _igcard = new IGCard();
-			if( id ) _igcard.render( id );
-		}
 	},
 	execute: function(callback, args, name) {
 		$(".page").hide();
